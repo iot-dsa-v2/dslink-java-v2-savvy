fc3df8b1f48b2d85f0b2fd7a321a736c7cafa6d2
Aaron <aaron_hansen@yahoo.com>
2017-09-30 10:22:25 -0700

    0.1.0

--- src/main/java/org/iot/dsa/dslink/example/ExampleRoot.java
+++ src/main/java/org/iot/dsa/dslink/example/ExampleRoot.java
@@ -26,9 +26,9 @@ public class ExampleRoot extends DSRootNode implements Runnable {
     // Fields
     ///////////////////////////////////////////////////////////////////////////
 
-    //Storing infos in fields eliminates name lookups, but should only be done
-    //with declared defaults.  This can be done with dynamic children, but extra
-    //care will be required.
+    // Storing infos in fields eliminates name lookups, but should only be done
+    // with declared defaults.  This can be done with dynamic children, but extra
+    // care will be required.
     private final DSInfo counter = getInfo(COUNTER);
     private final DSInfo reset = getInfo(RESET);
 
@@ -38,10 +38,19 @@ public class ExampleRoot extends DSRootNode implements Runnable {
     // Constructors
     ///////////////////////////////////////////////////////////////////////////
 
+    // Nodes must support the public no-arg constructor.  Technically this isn't required here
+    // since there are no other constructors...
+    public ExampleRoot() {
+    }
+
     ///////////////////////////////////////////////////////////////////////////
     // Methods
     ///////////////////////////////////////////////////////////////////////////
 
+    /**
+     * Defines the permanent children of this node type, their existence is guaranteed in all
+     * instances.  This is only ever called once per, type per process.
+     */
     @Override
     protected void declareDefaults() {
         super.declareDefaults();
@@ -53,19 +62,15 @@ public class ExampleRoot extends DSRootNode implements Runnable {
 
     /**
      * Handles the reset action.
-     *
-     * <p>
-     *
-     * {@inheritDoc}
      */
     @Override
     public ActionResult onInvoke(DSInfo actionInfo, ActionInvocation invocation) {
         if (actionInfo == reset) {
             synchronized (counter) {
                 put(counter, DSInt.valueOf(0));
-                //The following line would have also worked, but it would have
-                //required a name lookup.
-                //put(COUNTER, DSInt.valueOf(0));
+                // The following line would have also worked, but it would have
+                // required a name lookup.
+                // put(COUNTER, DSInt.valueOf(0));
             }
             return null;
         }
@@ -73,24 +78,16 @@ public class ExampleRoot extends DSRootNode implements Runnable {
     }
 
     /**
-     * Start the update timer.
-     *
-     * <p>
-     *
-     * {@inheritDoc}
+     * Starts the timer.
      */
     @Override
     protected void onSubscribed() {
-        //Use DSRuntime for timers and its thread pool.
+        // Use DSRuntime for timers and its thread pool.
         timer = DSRuntime.run(this, System.currentTimeMillis() + 1000l, 1000l);
     }
 
     /**
      * Cancels an active timer if there is one.
-     *
-     * <p>
-     *
-     * {@inheritDoc}
      */
     @Override
     protected void onStopped() {
@@ -101,11 +98,7 @@ public class ExampleRoot extends DSRootNode implements Runnable {
     }
 
     /**
-     * Cancel the update timer.
-     *
-     * <p>
-     *
-     * {@inheritDoc}
+     * Cancels the timer.
      */
     @Override
     protected void onUnsubscribed() {
@@ -122,10 +115,10 @@ public class ExampleRoot extends DSRootNode implements Runnable {
         synchronized (counter) {
             DSInt value = (DSInt) counter.getValue();
             put(counter, DSInt.valueOf(value.toInt() + 1));
-            //Without the counter field, this method would have required at least one lookup.
-            //The following is the worst performance option (not that it really matters here):
-            //DSInt val = (DSInt) get(COUNTER);
-            //put(COUNTER, DSInt.valueOf(val.toInt() + 1));
+            // Without the counter field, this method would have required at least one lookup.
+            // The following is the worst performance option (not that it really matters here):
+            // DSInt val = (DSInt) get(COUNTER);
+            // put(COUNTER, DSInt.valueOf(val.toInt() + 1));
         }
     }
 
